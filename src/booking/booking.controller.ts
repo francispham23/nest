1cef440ec7b8132687936ab0cc8da237eddcba72
Francis Pham <francis.pham.ca@gmail.com>
2025-02-11 22:59:51 -0800

    Route Parameters

--- src/booking/booking.controller.ts
+++ src/booking/booking.controller.ts
@@ -1,10 +1,22 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('booking')
 export class BookingController {
   @Get()
   findAll() {
     return 'All Bookings';
+  }
+  /* Single Route Parameter
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+      return `Booking ${id}`;
+    }
+  */
+
+  // Multiple Route Parameters
+  @Get(':id/:slug')
+  findOne(@Param('id') id: string, @Param('slug') slug: string) {
+    return `Booking ${id} and ${slug}`;
   }
 
   @Post()
